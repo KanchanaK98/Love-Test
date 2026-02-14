5307883935d9704a08340d7ec02a022a9b15aa49
Kanchana Kariyawasam <kanchana@geveo.com>
2026-02-14 21:58:38 +0530

    fix blank page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hey-modiz/",
+  base: "/Love-Test/",
   server: {
     host: "::",
     port: 8080,
